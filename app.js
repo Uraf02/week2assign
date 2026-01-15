af7718b345286b60a7a6ba26f94734bbf9970389
Usman Rafiq <usmanrafiq57@gmail.com>
2026-01-15 21:13:48 +0000

    loopforimagessave

---
+++ app.js
@@ -0,0 +1,59 @@
+const thumb = document.getElementById('thumbnails')
+
+const images = [
+    {
+        src: '.images/bonfire.png',
+        alt: 'bonfire'
+    },
+    {
+        src: '.images/CastleMoon.jpg',
+        alt: 'The moon over a castle'
+    },
+    {
+        src: '.images/Cathedral.jpg',
+        alt: 'Cathedral'
+    },
+    {
+        src: '.images/Eclipse.jpg',
+        alt: 'Eclipse'
+    },
+    {
+        src: '.images/inside.avif',
+        alt: 'The inside of a building'
+    },
+    {
+        src: '.images/redmoon.jpg',
+        alt: 'Red moon'
+    },
+    {
+        src: '.images/remains.jpg',
+        alt: 'Ruins'
+    }
+
+    
+]
+
+function thumbnailsmaking () {
+
+for(let i=0;i<images.length;i++) {
+    const imagelement= document.createElement('img')
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
