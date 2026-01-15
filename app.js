74ee077269a9752c4ebcf7e6bc3cdc16c098b86a
Usman Rafiq <usmanrafiq57@gmail.com>
2026-01-15 22:00:49 +0000

    imagesadded

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ const thumb = document.getElementById('thumbnails')
 
 const images = [
     {
-        src: '.images/bonfire.png',
+        src: './images/bonfire.png',
         alt: 'bonfire'
     },
     {
-        src: '.images/CastleMoon.jpg',
+        src: './images/CastleMoon.jpg',
         alt: 'The moon over a castle'
     },
     {
-        src: '.images/Cathedral.jpg',
+        src: './images/Cathedral.jpg',
         alt: 'Cathedral'
     },
     {
-        src: '.images/Eclipse.jpg',
+        src: './images/Eclipse.jpg',
         alt: 'Eclipse'
     },
     {
-        src: '.images/inside.avif',
+        src: './images/inside.avif',
         alt: 'The inside of a building'
     },
     {
-        src: '.images/redmoon.jpg',
+        src: './images/redmoon.jpg',
         alt: 'Red moon'
     },
     {
-        src: '.images/remains.jpg',
+        src: './images/remains.jpg',
         alt: 'Ruins'
     }
 
@@ -37,23 +37,21 @@ function thumbnailsmaking () {
 
 for(let i=0;i<images.length;i++) {
     const imagelement= document.createElement('img')
+    imagelement.src= images[i].src
+    imagelement.alt= images[i].alt
 
+    imagelement.addEventListener('click', function() {
+        console.log(image[1])
+    })
 
+    thumb.append(imagelement)
+}
 }
 
+thumbnailsmaking()
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-}
